91793cf3642110d67e0fbed51b2b37aa162cf65b
James Di Filippo <Jfilippo@paad.publicaffairs.ucla.edu>
2020-01-31 16:31:15 -0800

    added header and dependencies

---
+++ run_analyses.R
@@ -0,0 +1,15 @@
+# Meta script for EVgo Baseline Stats Reports
+# EVgo Evaluation Project
+# 2019
+
+# UCLA Luskin Center for Innovation
+# innovation.luskin.ucla.edu
+
+# James Di Filippo
+# jdifilippo@luskin.ucla.edu
+
+# dependencies, functions, options etc ----------------------------------------
+
+source("src/dependencies.r")
+
+options(stringsAsFactors = FALSE)

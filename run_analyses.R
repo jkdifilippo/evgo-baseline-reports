32a838139a9a666669f84f5e3128410527984ade
James Di Filippo <Jfilippo@paad.publicaffairs.ucla.edu>
2020-02-05 13:58:57 -0800

    added file structure

--- run_analyses.R
+++ run_analyses.R
@@ -1,15 +1,39 @@
-# Meta script for EVgo Baseline Stats Reports
-# EVgo Evaluation Project
-# 2019
+################################# HEADER #######################################
 
-# UCLA Luskin Center for Innovation
-# innovation.luskin.ucla.edu
+#   script: Automation script for EVgo Baseline Stats Reports
+#  project: EVgo Evaluation Project - Baseline Reports
+#     year: 2019
+#
+#   author: James Di Filippo
+#    email: jdifilippo@luskin.ucla.edu
+#
+#      org: UCLA Luskin Center for Innovation
+#  website: innovation.luskin.ucla.edu
 
-# James Di Filippo
-# jdifilippo@luskin.ucla.edu
-
-# dependencies, functions, options etc ----------------------------------------
-
+############################## DEPENDENCIES ####################################
 source("src/dependencies.r")
 
-options(stringsAsFactors = FALSE)
+########################### PROJECT STRUCTURE ##################################
+
+## builds file structure not contained in git repository
+folder_names <- 
+  c("data/processed",
+    "data/raw/shapefiles",
+    "data/raw/vehicle-data",
+    "output",
+    "output/datasets",
+    "reports")
+
+map(folder_names, dir.create, showWarnings = FALSE)
+
+################################ SCRIPTS #######################################
+
+
+
+
+########################### GENERATE REPORTS ###################################
+
+
+
+
+############################### CLEANUP ########################################
